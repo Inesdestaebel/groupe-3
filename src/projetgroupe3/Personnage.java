13843608e46681ea84cfd0c1fb7bc36e2a7610c7
Hugo <Hugo@Mikasa>
2021-03-05 19:29:31 +0100

    Ajout de l'attribut position Petite methode (à la fin en comm) à essayer d'integrer à Plateaudejeu

--- src/projetgroupe3/Personnage.java
+++ src/projetgroupe3/Personnage.java
@@ -1,14 +1,32 @@
 package projetgroupe3;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Personnage {
 	private String name;
 	private int pV=5;
 	private ArrayList<Item>inventaire= new ArrayList<Item>();
 	private boolean alive=true;
+	private int[] position= new int[2];
 	
 	
+	
+
+	public Personnage(String name, int[]position) {
+		setName(name);
+		setPosition(position);
+	}
+
+	public int[] getPosition() {
+		return position;
+	}
+
+	public void setPosition(int[] position) {
+		this.position = position;
+	}
+
 	
 	public String getName() {
 		return name;
@@ -24,8 +42,8 @@ public class Personnage {
 		return pV;
 	}
 	
-	public void setPV(int PV) {
-		pV = PV;
+	public void setPV(int pv) {
+		pV = pv;
 	}
 
 
@@ -44,6 +62,7 @@ public class Personnage {
 	}
 	
 	// permet au Personnage de se soigner s’il possède une potion dans son inventaire
+	
 	public void UsePotion() {
 		int heal=3;
 		int i=0;
@@ -56,6 +75,31 @@ public class Personnage {
 		}
 	}
 
+	
+	
+	/*
+	//Pour la classe Plateaudejeu j'ai pensé à ça
+	//petite methode pour ajouter un Peronnage dans une partie sur un Plateaudejeu 
+	public Personnage addPlayer {
+		Scanner sc= new Scanner(System.in);
+		Random r = new Random();
+		int x=r.nextInt(this.X);
+		int y=r.nextInt(this.Y);
+		while (plateau[x][y]!='?') {
+			int x=r.nextInt(this.X);
+			int y=r.nextInt(this.Y);	
+		}
+		int[]pos= {x,y};
+		System.out.println("Votre nom de Personnage");
+		String name=sc.next();
+		Personnage player=new Personnage(name,pos );
+		this.plateau[x][y]='H' //H designera le personnage lors de l'affichage du plateau.
+		sc.close();
+		return player;
+	}
+	
+	
+	*/
 	
 
 }
