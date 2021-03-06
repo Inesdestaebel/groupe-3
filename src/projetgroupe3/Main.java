1e010bcc6d234d354f79deeb14fe1d3bed8a7526
Inesdestaebel <ines09des@gmail.com>
2021-03-06 14:12:12 +0100

    main plateau

--- src/projetgroupe3/Main.java
+++ src/projetgroupe3/Main.java
@@ -6,13 +6,12 @@ public class Main {
 		int X = 15;
 		int Y = 9;
 		Plateaudejeu plateau = new Plateaudejeu(X,Y);
-		for ( int y = 0 ; y<Y; y++ ) {
-			for ( int x = 0 ; x<X; x++ ) {
-				System.out.print(plateau[x][y]);
-		}
+		for ( int x = 0 ; x<X; x++ )
+			for ( int y = 0 ; y<Y; y++ )
+				System.out.println(plateau.getPlateau(x, y));
+		
 		System.out.println();
 		}
-
-	}
-
 }
+
+
--- src/projetgroupe3/Plateaudejeu.java
+++ src/projetgroupe3/Plateaudejeu.java
@@ -9,69 +9,93 @@ public class Plateaudejeu {
 		private char[][] plateau = new char[X][Y];
 		
 		public Plateaudejeu(int X, int Y) {
-			setPlateau(plateau);
+			setX(X);
+			setY(Y);
+			setPlateau(X,Y);
 		}
 		
-		public char[][] getPlateau() {
-			return plateau;
+		public int getX() {
+			return X;
 		}
 		
-		public void setPlateau(char[][] plateau ) {
+		public void setX(int X) {
+			this.X=X;	
+		}
+		
+		public int getY() {
+			return Y;
+		}
+		
+		public void setY(int Y) {
+				this.Y=Y;	
+		}
+		
+		public char getPlateau(int x, int y) {
+				return plateau[x][y];
+			
+		}
+		
+		public void setPlateau(int X, int Y) {
+			try {
 			for ( int x = 0 ; x<X; x++ )
 				for ( int y = 0 ; y<Y; y++ )
 					plateau[x][y]= '?';
 		
-		//mise en place des cases sur le plateau 
+			//mise en place des cases sur le plateau 
 		
-		Random r = new Random();
+			Random r = new Random();
 		
-		//placement de la case objectif
-		int v1 = r.nextInt(15); 
-		int v2 = r.nextInt(9);
-		plateau[v1][v2]='V';
+			//placement de la case objectif
+			int v1 = r.nextInt(X); 
+			int v2 = r.nextInt(Y);
+			plateau[v1][v2]='V';
 		
-		//placement des cases vides
-		int vides = 60;
-		while(vides>0) {
-			int a = r.nextInt(15);
-			int b = r.nextInt(9);
-			if(plateau[a][b]=='?') {
-				plateau[a][b]=' ';	
-				vides = vides-1;
+			//placement des cases vides
+			int vides = 60;
+			while(vides>0) {
+				int a = r.nextInt(X);
+				int b = r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]=' ';	
+					vides = vides-1;
+				}
+			}
+		
+			//placement des cases murs
+			int murs = 44;
+			while(murs>0) {
+				int a=r.nextInt(X);
+				int b=r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]= '#';
+					murs = murs-1;
+				}
+			}
+		
+			//placement des cases piège
+			int piège = 15;
+			while(piège>0) {
+				int a=r.nextInt(X);
+				int b=r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]= '~';
+					piège = piège-1;
+				}
+			}
+		
+			//placement des cases potion
+			int potion = 15;
+			while(potion>0) {
+				int a=r.nextInt(X);
+				int b=r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]= 'P';
+					potion = potion-1;
+				}
+			}
+			}catch(ArrayIndexOutOfBoundsException ex) {
+				System.out.println("Veuillez rentrer un X et un Y positif.");
 			}
 		}
 		
-		//placement des cases murs
-		int murs = 44;
-		while(murs>0) {
-			int a=r.nextInt(15);
-			int b=r.nextInt(9);
-			if(plateau[a][b]=='?') {
-				plateau[a][b]= '#';
-				murs = murs-1;
-			}
-		}
-		
-		//placement des cases piège
-		int piège = 15;
-		while(piège>0) {
-			int a=r.nextInt(15);
-			int b=r.nextInt(9);
-			if(plateau[a][b]=='?') {
-				plateau[a][b]= '~';
-				piège = piège-1;
-			}
-		}
-		
-		//placement des cases potion
-		int potion = 15;
-		while(potion>0) {
-			int a=r.nextInt(15);
-			int b=r.nextInt(9);
-			if(plateau[a][b]=='?') {
-				plateau[a][b]= 'P';
-				potion = potion-1;
-			}
-		}
-}
 }
