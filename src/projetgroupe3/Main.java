5779009e70f72baa22dbb66dd6e8f4bc02612526
Inesdestaebel <ines09des@gmail.com>
2021-03-07 22:17:05 +0100

    Classe plateau avec l'affichage qui fonctionne Ajout de la fonction addplayer faite par Hugo Début du main pour l'accueil du joueur

--- src/projetgroupe3/Main.java
+++ src/projetgroupe3/Main.java
@@ -3,15 +3,44 @@ package projetgroupe3;
 public class Main {
 
 	public static void main(String[] args) {
-		int X = 15;
-		int Y = 9;
-		Plateaudejeu plateau = new Plateaudejeu(X,Y);
-		for ( int x = 0 ; x<X; x++ )
-			for ( int y = 0 ; y<Y; y++ )
-				System.out.println(plateau.getPlateau(x, y));
-		
-		System.out.println();
+		int X = 9;
+		int Y = 15;
+		int vides = 60;
+		int murs = 44;
+		int pieges = 15;
+		int potion = 15;
+		Plateaudejeu plateau = new Plateaudejeu(X,Y,vides,murs,pieges,potion);
+		if(X*Y!=vides+murs+pieges+potion+1) {
+			System.out.println("Certaines cases ne sont pas attribuées.");
 		}
+		//Pour afficher le plateau mais à supprimer dans le jeu car le joueur ne doit pas le voir au début.
+		//plateau.afficher();
+		
+		//Ajout du joueur.
+		plateau.addPlayer();
+		
+	
+		//plateau.afficher();
+		
+		//Plateau que le joueur verra, on changera les valeurs en introduisant les valeurs 
+		//DOIS JE METTRE CA DANS UNE AUTRE CLASSE?
+		char[][] plateaujoueur = new char[X][Y];
+		for ( int x = 0 ; x<X; x++ ) {
+			for ( int y = 0 ; y<Y; y++ ) {
+				if (plateau.valeurcase(x, y)=='H') {
+					plateaujoueur[x][y]='H';
+				}
+				else {
+				plateaujoueur[x][y]= '?';
+				}
+				System.out.print(plateaujoueur[x][y]);
+			}
+			System.out.println();
+			
+		
+
+		}
+}
 }
 
 
--- src/projetgroupe3/Plateaudejeu.java
+++ src/projetgroupe3/Plateaudejeu.java
@@ -1,17 +1,72 @@
 package projetgroupe3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Plateaudejeu {
 		private int X;
 		private int Y;
-		private char[][] plateau = new char[X][Y];
+		private char[][] plateau;
 		
-		public Plateaudejeu(int X, int Y) {
+		//Constructeur
+		public Plateaudejeu(int X, int Y, int vides, int murs, int pieges, int potion) {
 			setX(X);
 			setY(Y);
-			setPlateau(X,Y);
-		}
+			plateau = new char[X][Y];
+			
+			for ( int x = 0 ; x<X; x++ )
+				for ( int y = 0 ; y<Y; y++ )
+					plateau[x][y]= '?';
+			
+			//mise en place des cases sur le plateau 
+			
+			Random r = new Random();
+			
+			//placement de la case objectif
+			int v1 = r.nextInt(X); 
+			int v2 = r.nextInt(Y);
+			plateau[v1][v2]='V';
+			
+			//placement des cases vides
+			while(vides>0) {
+				int a = r.nextInt(X);
+				int b = r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]=' ';	
+					vides = vides-1;
+				}
+			}
+			
+			//placement des cases murs
+			while(murs>0) {
+				int a=r.nextInt(X);
+				int b=r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]= '#';
+					murs = murs-1;
+				}
+			}
+			
+			//placement des cases piège
+			while(pieges>0) {
+				int a=r.nextInt(X);
+				int b=r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]= '~';
+					pieges = pieges-1;
+				}
+			}
+			
+			//placement des cases potion
+			while(potion>0) {
+				int a=r.nextInt(X);
+				int b=r.nextInt(Y);
+				if(plateau[a][b]=='?') {
+					plateau[a][b]= 'P';
+					potion = potion-1;
+				}
+			}
+	}
 		
 		public int getX() {
 			return X;
@@ -29,72 +84,37 @@ public class Plateaudejeu {
 				this.Y=Y;	
 		}
 		
-		public char getPlateau(int x, int y) {
-				return plateau[x][y];
-			
+		
+		public void afficher() {
+
+			for ( int x = 0 ; x<X; x++ ) {
+				for ( int y = 0 ; y<Y; y++ ) {
+					System.out.print(plateau[x][y]);
+				}
+			System.out.println();
+			}
+		}
+
+		public Personnage addPlayer() {
+			Scanner sc= new Scanner(System.in);
+			Random r = new Random();
+			int x=r.nextInt(this.X);
+			int y=r.nextInt(this.Y);
+			while (plateau[x][y]!=' ') {
+				 x=r.nextInt(this.X);
+				 y=r.nextInt(this.Y);	
+			}
+			int[]pos= {x,y};
+			System.out.println("Entrez votre nom de Personnage :");
+			String name=sc.next();
+			Personnage player=new Personnage(name,pos);
+			this.plateau[x][y]='H'; //H designera le personnage lors de l'affichage du plateau.
+			sc.close();
+			return player;
 		}
 		
-		public void setPlateau(int X, int Y) {
-			try {
-			for ( int x = 0 ; x<X; x++ )
-				for ( int y = 0 ; y<Y; y++ )
-					plateau[x][y]= '?';
-		
-			//mise en place des cases sur le plateau 
-		
-			Random r = new Random();
-		
-			//placement de la case objectif
-			int v1 = r.nextInt(X); 
-			int v2 = r.nextInt(Y);
-			plateau[v1][v2]='V';
-		
-			//placement des cases vides
-			int vides = 60;
-			while(vides>0) {
-				int a = r.nextInt(X);
-				int b = r.nextInt(Y);
-				if(plateau[a][b]=='?') {
-					plateau[a][b]=' ';	
-					vides = vides-1;
-				}
-			}
-		
-			//placement des cases murs
-			int murs = 44;
-			while(murs>0) {
-				int a=r.nextInt(X);
-				int b=r.nextInt(Y);
-				if(plateau[a][b]=='?') {
-					plateau[a][b]= '#';
-					murs = murs-1;
-				}
-			}
-		
-			//placement des cases piège
-			int piège = 15;
-			while(piège>0) {
-				int a=r.nextInt(X);
-				int b=r.nextInt(Y);
-				if(plateau[a][b]=='?') {
-					plateau[a][b]= '~';
-					piège = piège-1;
-				}
-			}
-		
-			//placement des cases potion
-			int potion = 15;
-			while(potion>0) {
-				int a=r.nextInt(X);
-				int b=r.nextInt(Y);
-				if(plateau[a][b]=='?') {
-					plateau[a][b]= 'P';
-					potion = potion-1;
-				}
-			}
-			}catch(ArrayIndexOutOfBoundsException ex) {
-				System.out.println("Veuillez rentrer un X et un Y positif.");
-			}
+		public char valeurcase (int x, int y) {
+			return plateau[x][y];
 		}
 		
 }
